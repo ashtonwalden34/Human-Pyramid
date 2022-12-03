9d594e725e4091e74aba90f9ea984b85b5dd8bcc
Ashton Walden <ashtonwalden@icloud.com>
2022-12-02 23:13:17 -0700

    set up complete

--- pyramid.py
+++ pyramid.py
@@ -1,1 +1,34 @@
-# test
+# input will be num of rows
+
+rows = 0
+
+def weight_on(row, col):
+    if row == 0:
+        result = 0
+    # outer left
+    elif col == 0:
+        #  what the video showed
+        result = (200 + weight_on(row - 1, 0)) / 2
+        #  what I think may be correct
+        result = 200 + (weight_on(row - 1, 0) / 2)
+    # outer right
+    elif col == row:
+         #  what the video showed
+        result = (200 + weight_on(row - 1, row)) / 2
+        #  what I think may be correct
+        result = 200 + (weight_on(row - 1, row) / 2)
+    # middle columns
+    else:
+         #  what the video showed
+        result = (200 + (weight_on(row - 1, col -1) + weight_on(row - 1, col)) / 2)
+        #  what I think may be correct
+        result = 200 + (weight_on(row - 1, col -1) + weight_on(row - 1, col) / 2)
+        pass
+
+
+for row in range(rows):
+    # num of cols = num of row + 1
+    for col in range(rows + 1):
+        print(weight_on(row, col))
+    # print new line
+    print()
