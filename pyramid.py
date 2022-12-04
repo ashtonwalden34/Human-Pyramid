dbd2d1f6f588bf7bbc37af1a27b11599dad5e81c
ashtonwalden34 <ashtonwalden34@gmail.com>
2022-12-03 18:20:57 -0700

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,26 +1,27 @@
 # input will be num of rows
-
-rows = 0
+rows = int(input("Enter a the number of rows: "))
 
 def weight_on(row, col):
+    col = int(col)
+    row = int(row)
     if row == 0:
         result = 0
     # outer left
     elif col == 0:
         #  what the video showed
-        result = (200 + weight_on(row - 1, 0)) / 2
+        # result = (200 + weight_on(row - 1, 0)) / 2
         #  what I think may be correct
         result = 200 + (weight_on(row - 1, 0) / 2)
     # outer right
     elif col == row:
          #  what the video showed
-        result = (200 + weight_on(row - 1, row)) / 2
+        # result = (200 + weight_on(row - 1, row)) / 2
         #  what I think may be correct
         result = 200 + (weight_on(row - 1, row) / 2)
     # middle columns
     else:
          #  what the video showed
-        result = (200 + (weight_on(row - 1, col -1) + weight_on(row - 1, col)) / 2)
+        # result = (200 + (weight_on(row - 1, col -1) + weight_on(row - 1, col)) / 2)
         #  what I think may be correct
         result = 200 + (weight_on(row - 1, col -1) + weight_on(row - 1, col) / 2)
         pass
